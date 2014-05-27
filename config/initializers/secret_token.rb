e809a6a2395ca7f425c69edaf4c8213555a31ede
HONGSEOK <hongs@jung2poom.cafe24.com>
2014-05-27 16:29:40 +0900

    first add

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VetnoteWeb::Application.config.secret_token = '09a9823988cb53db7c03c3b0ce7806ec6d94d4202781c7c452d33108b9300b9e7484e21e31f14745823c692b2f529812dcd4c08d766e8418d03bdb6579be501a'
